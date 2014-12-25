72b86c68aa0bb7f54d2bb9df46e857706e32907a
Istvan Csaki <istvan@drupart.hu>
2014-12-25 09:01:51 +0100

    Remove flex slider

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,9 +1,3 @@
 $(window).load(function() {
-/*    $('.flexslider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        slideshowSpeed: 10000,
-        pauseOnHover: true,
-        pauseOnAction: true
-    }); */
+
 });
