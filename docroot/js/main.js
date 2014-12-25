bfc53cedb1731f1eea9a19b158648b5b343ce8a0
tompagabor <gabor.tompa@gmail.com>
2014-12-25 06:01:44 +0100

    turn off slider, and make 2 column summary

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,9 +1,9 @@
 $(window).load(function() {
-    $('.flexslider').flexslider({
+/*    $('.flexslider').flexslider({
         animation: "slide",
         directionNav: false,
         slideshowSpeed: 10000,
         pauseOnHover: true,
         pauseOnAction: true
-    });
+    }); */
 });
